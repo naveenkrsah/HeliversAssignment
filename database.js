0bc198aeaedaaf7273e9ea18408f78c769c68620
naveenkrsah <naveensah3010@gmail.com>
2024-04-04 17:18:09 +0530

    changes

--- database.js
+++ database.js
@@ -7,6 +7,7 @@ const mongoUrl = process.env.REACT_APP_MONGODB_URL;
 // const mongoURL = `${mongoUrl}/heliverse`;
 // const mongoURL = "mongodb://127.0.0.1:27017/heliverse";
 const mongoURL = "mongodb+srv://coderdost:h9z77jIi5CDeT8Ne@cluster0.sld6bhl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+// const mongoURL = "mongodb+srv://coderdost:h9z77jIi5CDeT8Ne@cluster0.sld6bhl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 // MongoDB connection options
 const options = {
